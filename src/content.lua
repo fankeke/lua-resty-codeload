b52db7ed83b5a8e60488bc938d56cdb927d03302
fankeke <382482175@qq.com>
2017-05-08 03:00:00 -0700

    test tab

--- src/content.lua
+++ src/content.lua
@@ -2,6 +2,9 @@
 local rload 	= require "mload.lib.resty.load"
 local rload     = require "mload.lib.resty.load"
 
+local	hello
+local   hello
+
 local config = {
 	load_init = {
 		module_name = "foo"
